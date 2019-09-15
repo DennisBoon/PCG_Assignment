38dead46017f9c954a37f823388103b7c6e45bb3
Dennis Boon <dennis.boon96@gmail.com>
2019-09-15 21:07:15 +0200

    Progress

---
+++ Procedural Level Design/Assets/Scripts/New Scripts/BoardManager.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int boardRows, boardColumns;
+    public int minRoomSize, maxRoomSize;
+    public GameObject floorTile;
+    private GameObject[,] boardPositionsFloor;
+
+    public class SubDungeon
+    {
+        public SubDungeon left, right;
+        public Rect rect;
+        public Rect room = new Rect(-1, -1, 0, 0);
+        public int debugId;
+
+        private static int debugCounter = 0;
+
+        public SubDungeon(Rect mrect)
+        {
+            rect = mrect;
+            debugId = debugCounter;
+            debugCounter++;
+        }
+
+        public bool IAmLeaf()
+        {
+            return left == null && right == null;
+        }
+
+        public bool Split(int minRoomSize, int maxRoomSize)
+        {
+            if (!IAmLeaf())
+            {
+                return false;
+            }
+
+            bool splitH;
+            if (rect.width / rect.height >= 1.25)
+            {
+                splitH = false;
+            }
+            else if (rect.height / rect.width >= 1.25)
+            {
+                splitH = true;
+            }
+            else
+            {
+                splitH = Random.Range(0.0f, 1.0f) > 0.5;
+            }
+
+            if (Mathf.Min(rect.height, rect.width) / 2 < minRoomSize)
+            {
+                Debug.Log("Sub-dungeon " + debugId + " will be a leaf");
+                return false;
+            }
+
+            if (splitH)
+            {
+                int split = Random.Range(minRoomSize, (int)(rect.width - minRoomSize));
+
+                left = new SubDungeon(new Rect(rect.x, rect.y, rect.width, split));
+                right = new SubDungeon(new Rect(rect.x, rect.y + split, rect.width, rect.height - split));
+            }
+            else
+            {
+                int split = Random.Range(minRoomSize, (int)(rect.height - minRoomSize));
+
+                left = new SubDungeon(new Rect(rect.x, rect.y, split, rect.height));
+                right = new SubDungeon(new Rect(rect.x + split, rect.y, rect.width - split, rect.height));
+            }
+
+            return true;
+        }
+
+        public void CreateRoom()
+        {
+            if (left != null)
+            {
+                left.CreateRoom();
+            }
+            if (right != null)
+            {
+                right.CreateRoom();
+            }
+            if (IAmLeaf())
+            {
+                int roomWidth = (int)Random.Range(rect.width / 2, rect.width - 2);
+                int roomHeight = (int)Random.Range(rect.height / 2, rect.height - 2);
+                int roomX = (int)Random.Range(1, rect.width - room.width - 1);
+                int roomY = (int)Random.Range(1, rect.height - roomHeight - 1);
+
+                room = new Rect(rect.x + roomX, rect.y + roomY, roomWidth, roomHeight);
+                Debug.Log("Created room " + room + " in sub-dungeon " + debugId + " " + rect);
+            }
+        }
+    }
+
+    public void CreateBSP(SubDungeon subDungeon)
+    {
+        Debug.Log("Splitting sub-dungeon " + subDungeon.debugId + ": " + subDungeon.rect);
+        if (subDungeon.IAmLeaf())
+        {
+            if (subDungeon.rect.width > maxRoomSize ||
+                subDungeon.rect.height > maxRoomSize ||
+                Random.Range(0.0f, 1.0f) > 0.25)
+            {
+                if (subDungeon.Split(minRoomSize, maxRoomSize))
+                {
+                    Debug.Log("Splitted sub-dungeon " + subDungeon.debugId + " in "
+                        + subDungeon.left.debugId + ": " + subDungeon.left.rect + ", "
+                        + subDungeon.right.debugId + ": " + subDungeon.right.rect);
+
+                    CreateBSP(subDungeon.left);
+                    CreateBSP(subDungeon.right);
+                }
+            }
+        }
+    }
+
+    public void DrawRooms(SubDungeon subDungeon)
+    {
+        if (subDungeon == null)
+        {
+            return;
+        }
+        if (subDungeon.IAmLeaf())
+        {
+            for (int i = (int)subDungeon.room.x; i < subDungeon.room.xMax; i++)
+            {
+                for (int j = (int)subDungeon.room.y; j < subDungeon.room.yMax; j++)
+                {
+                    GameObject instance = Instantiate(floorTile, new Vector3(i, j, 0f), Quaternion.identity);
+                    instance.transform.SetParent(transform);
+                    boardPositionsFloor[i, j] = instance;
+                }
+            }
+        }
+        else
+        {
+            DrawRooms(subDungeon.left);
+            DrawRooms(subDungeon.right);
+        }
+    }
+
+    void Start()
+    {
+        SubDungeon rootSubDungeon = new SubDungeon(new Rect(0, 0, boardRows, boardColumns));
+        CreateBSP(rootSubDungeon);
+        rootSubDungeon.CreateRoom();
+        boardPositionsFloor = new GameObject[boardRows, boardColumns];
+        DrawRooms(rootSubDungeon);
+    }
+}
